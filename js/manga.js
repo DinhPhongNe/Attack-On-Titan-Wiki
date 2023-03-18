0017852de068e1eb439ab8f7f1c472c0d0e4a29e
DinhPhongNe <phongvo2405@example.com>
2023-03-18 22:03:07 +0700

    update new things

---
+++ js/manga.js
@@ -0,0 +1,51 @@
+var swiper = new Swiper(".product-slider", {
+    slidesPerView: 3,
+    loop:true,
+    spaceBetween: 10,
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        550: {
+          slidesPerView: 2,
+        },
+        800: {
+          slidesPerView: 3,
+        },
+        1000: {
+            slidesPerView: 3,
+        },
+    },
+});
+
+var swiper = new Swiper(".review-slider", {
+    slidesPerView: 3,
+    loop:true,
+    spaceBetween: 10,
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        550: {
+          slidesPerView: 2,
+        },
+        800: {
+          slidesPerView: 3,
+        },
+        1000: {
+            slidesPerView: 3,
+        },
+    },
+});
