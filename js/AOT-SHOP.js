403a8fb76da34931f4e9d9fd4aae211f4d19f811
DinhPhongNe <phongvo2405@example.com>
2023-03-21 21:40:48 +0700

    update

--- js/AOT-SHOP.js
+++ js/AOT-SHOP.js
@@ -49,7 +49,7 @@ document.querySelectorAll('.small-image-3').forEach(images =>{
     }
 });
 
-let countDate = new Date('aug 1, 2021 00:00:00').getTime();
+let countDate = new Date('aug 1, 2023 00:00:00').getTime();
 
 function countDown(){
 
